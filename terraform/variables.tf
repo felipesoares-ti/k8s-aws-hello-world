3bb3939345dd5b73201aaf3336ddada21991fcb0
Felipe Almeida Soares <64097720+felipesoares-ti@users.noreply.github.com>
2024-04-19 16:21:24 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,25 @@
+# variables.tf
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+  default = "k8s-vpc"
+}
+
+variable "subnet_name" {
+  type = string
+  default = "k8s-subnet"
+}
+
+variable "key_pair_name" {
+  type = string
+  default = "k8s-key-pair"
+}
 
